1fb26c52c25e0234e23a81f8835c0e071c990cbc
Tiago Rafael <tiago.rafael019@gmail.com>
2023-08-28 09:59:57 -0300

    Enviando API do WhatsApp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import  { Menu }  from './components/Menu/Menu'
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import ReactWhatsapp from 'react-whatsapp';
 
 function App() {
+
+  const number = "(85) 98780-5592"
+
+  const message = "helo, word!";
 
   return (
 
     <div className="App">
-      <Menu/>
+      <div className="content">
+        <h1 className="title">React Whatsapp</h1>
+          <section className="library">
+            <input
+              id="number"
+              placeholder="Number"
+              value={number}
+            />
+            <input
+              id="message"
+              placeholder="Message"
+              value={message}
+            />
+            <ReactWhatsapp number={number} message={message}>
+              Open Whatsapp
+            </ReactWhatsapp>
+          </section>
+      </div>
     </div>
+
   );
 }
 
