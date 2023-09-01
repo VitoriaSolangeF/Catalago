99dc847c3882a8fab65d9739612892bdd6bdfbbd
Tiago Rafael <tiago.rafael019@gmail.com>
2023-09-01 13:43:17 -0300

    Layout da categoria pronto

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -11,21 +11,30 @@ function ProductCard({ produtos, busca }) {
     }, [produtos, busca]);
     
     return (
-        <div className="container">
-            {produtsFiltradas.map((produto) => (
-                <section key={produto.id} className="product-card">
-                    <img src={produto.urlImage} alt="fone" className="card__image" />
+            
+                <div className="container">
+                    {produtsFiltradas.map((produto) => (
+                        <div key={produto.id} className="product-card w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <a href="#">
+                                <img className="p-8 rounded-t-lg" src="https://png.pngitem.com/pimgs/s/134-1341378_apple-watch-series-4-hd-png-download.png"/>
+                            </a>
+                            <div className="card_infos px-5 pb-5">
+                                <a href="#">
+                                    <h5 className="descricao text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{produto.description}</h5>
+                                </a>
+                                <div className="container_card flex">
+                                    <span className="valor">R${produto.value}</span>
+                                    <span className="info">ID:{produto.id}</span>
+                                    <span className="info">Estoque: 10</span>
+                                    <span className="info">Cx: {produto.cx_und}</span>
+                                    <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">WhatApp</a>
+                                </div>
 
-                    <div className="card__infos">
-                        <h2 className="descricao">{produto.description}</h2>
-                        <p className="valor">Atacado: R${produto.value}</p>
-                        <p className="id">ID:{produto.id}</p>
-                        <p className="stock">Estoque: {produto.stock}</p>
-                        <p className="cx_und">Cx: {produto.cx_und}</p>
-                    </div>
-                </section>
-            ))}
-        </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
     );
 }
 export default ProductCard;
