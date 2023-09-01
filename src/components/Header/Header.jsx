67c4c027f20a3eff5d320a95466be5ed5bcb60c5
Tiago Rafael <tiago.rafael019@gmail.com>
2023-09-01 10:48:57 -0300

    Input de pesquisa consertado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,32 +3,30 @@ import './Header.css';
 import CartButton from "../CartButton/CartButton";
 import Menu from "../Menu/Menu";
 
-
-
-<<<<<<< HEAD
 function Header({valor, onChange}) {
 
-=======
-
-function Header() {
->>>>>>> 1b21e4b0f9e53ab16f4484358c45c8d9fb7aada0
     return (
         <header className="hearder">
             <div className="container_menu">
                 <img className="logo" src="logobranca.png" alt="logo__kimaster" />
-<<<<<<< HEAD
-                <input
-                    type="text"
-                    placeholder="Pesquisar produtos..."
-                    value={valor}
-                    onChange={onChange}
-                />
+                <div className="input_search">
+                        <div className="search">
+                            <input
+                                type="search"
+                                id="default-search"
+                                className="p-4 pl-10 text-sm border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-black-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Pesquisar produtos..."
+                                value={valor}
+                                onChange={onChange}
+                            />
+                            <div className="icon-search">
+                                <ion-icon name="search-outline"></ion-icon>
+                            </div>
+                        </div>
+    
+                </div>
+                <Menu/> 
                 <CartButton />
-                <Menu />
-=======
-                <SearchBar />
-                <Menu/>
->>>>>>> 1b21e4b0f9e53ab16f4484358c45c8d9fb7aada0
             </div>
         </header>
     );
