48a0d91fb5dd8db03c88da2660e3485fb92fd84d
Tiago Rafael <tiago.rafael019@gmail.com>
2023-09-01 15:32:17 -0300

    Layout dos produtos finalizados

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./components/Header/Header";
 import Banner from "./components/Banner/Banner";
@@ -6,34 +6,52 @@ import ProductCard from "./components/ProductCard/ProductCard";
 import Carregadores from "./components/Categorys/Categorys";
 import Banner2 from "./components/Banner2/Banner2";
 import { LoadProdutos } from './components/utils/loadProdutos'; 
+import whatapp from './assets/whatsapp.png'
 
 function App() {
-
   const [busca, setBusca] = useState('');
   const produtos = LoadProdutos();
 
-  console.log(busca)
+  useEffect(() => {
+    const isPageReloaded = localStorage.getItem('isPageReloaded');
+    if (!isPageReloaded) {
+      localStorage.setItem('isPageReloaded', 'true');
+    } else {
+      localStorage.removeItem('isPageReloaded');
+      document.querySelector('.content').classList.add('pulse-animation');
+    }
+  }, []);
 
   return (
     <div className='app'>
         <Header value={busca} onChange={(e) => setBusca(e.target.value)} />
           <div className="Banner">
             <Banner />
+          </div> 
 
           <div className="Banner2">
-            <Banner2 />.
+            <Banner2 />
           </div>
 
           <div className="Carregadores">
             <Carregadores produtos={produtos} busca={busca}/>.
           </div>
 
-          </div>
           <div className="card-container">
             <ProductCard produtos={produtos} busca={busca}  />
+
+            <div className="content"> {/* Aplicar a classe dinamicamente */}
+              <img src={whatapp} alt="" />
+            </div>
           </div>
       </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
